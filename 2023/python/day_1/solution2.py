01053f2b9c83a0bfd17fe46ff6b696f17a41886a
Austin Nieset <ajnieset@gmail.com>
2023-12-01 21:28:25 -0500

    day 1 part 2

--- 2023/python/day_1/solution2.py
+++ 2023/python/day_1/solution2.py
@@ -4,45 +4,38 @@ VALID_DIGITS = ["one", "two", "three", "four", "five", "six", "seven", "eight", 
 
 def get_input() -> list[str]:
     input = []
-    with open("input2.csv", "r") as file:
+    with open("input.csv", "r") as file:
         for line in file:
             input.append(str(line).strip())
     
     return input
 
-def solve(input: list[str]):
-    converted = []
+def solve(input: list[str]) -> int:
+    total = 0
+
     for line in input:
-        og_line = line
-        first = 9999
-        last = -1
-        first_word = None
-        first_digit = None
-        last_word = None
-        last_digit = None
-        for digit, word in enumerate(VALID_DIGITS, start=1):
-            found = line.find(word)
-            if first > found and found != -1:
-                first = found
-                first_word = word
-                first_digit = str(digit)
-            if last < found:
-                last = found
-                last_word = word
-                last_digit = str(digit)
-
-        if first_word is not None and first_digit is not None:
-            line = line.replace(first_word, first_digit)
-        if last_word is not None and last_digit is not None:
-            line = line.replace(last_word, last_digit)
-
-        for digit, word in enumerate(VALID_DIGITS, start=1):
-            line = line.replace(word, str(digit))
-
-        print(f"{og_line = } {line = }")
-        converted.append(line)
-
-    return part_1(converted)
+        first = None
+        last = None
+        for i in range(len(line)):
+            if line[i].isdigit():
+                if first is None:
+                    first = int(line[i])
+                else:
+                    last = int(line[i])
+            else:
+                for digit, word in enumerate(VALID_DIGITS, start=1):
+                    if line[i:].startswith(word):
+                        if first is None:
+                            first = digit
+                        else:
+                            last = digit
+        if last is None:
+            value = int(f"{first}{first}")
+        else:
+            value = int(f"{first}{last}")
+        
+        total += value
+    return total
 
 if __name__ == "__main__":
     inputs = get_input()
